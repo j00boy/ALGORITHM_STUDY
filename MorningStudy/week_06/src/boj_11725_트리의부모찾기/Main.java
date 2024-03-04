cd7bbb752a8b4242d3cf306bd50589a9789554e2
j00boy <kkjjhh9705@gmail.com>
2024-03-04 17:37:55 +0900

    week 6

--- MorningStudy/week_06/src/boj_11725_트리의부모찾기/Main.java
+++ MorningStudy/week_06/src/boj_11725_트리의부모찾기/Main.java
@@ -1,5 +1,24 @@
 package boj_11725_트리의부모찾기;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	
+	static int root = 1;
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for(int i = 2; i <= N; i++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			
+			
+		}
+		
+		
+	}
 }
