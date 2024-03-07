4d30f691cb566dcbc1a0227080bdae432753e1e7
j00boy <kkjjhh9705@gmail.com>
2024-03-07 09:28:31 +0900

    숨바꼭질 done

--- algorithm-by_myself/240305/src/boj_1697_숨바꼭질/Main2.java
+++ algorithm-by_myself/240305/src/boj_1697_숨바꼭질/Main2.java
@@ -4,18 +4,16 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class Main {
+public class Main2 {
 	
 	static int N;		// 수빈이
 	static int K;		// 수빈이 동생
 	static int[] road;
-	static boolean[] visited;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		road = new int[100001];
-		visited = new boolean[100001];
 
 		N = sc.nextInt();
 		K = sc.nextInt();
@@ -33,25 +31,23 @@ public class Main {
 		Queue<Integer> queue = new LinkedList<>();
 		
 		queue.add(start);
-		visited[start] = true;
 		road[start] = 0;
 		
-		while(!visited[K]) {
+		while(!queue.isEmpty()) {
+			
 			int num = queue.poll();
 			
 			int[] arr = {num-1, num+1, num*2};
 			
 			for(int i : arr) {
-				if(check(i) && !visited[i]) {
-					visited[i] = true;
+				if(i == start) {
+					continue;
+				}
+				if(i >= 0 && i <= 100000 && road[i] == 0) {
 					queue.add(i);
 					road[i] = road[num]+1;
 				}
 			}
 		}
-	}
-
-	public static boolean check(int n) {
-		return n >= 1 && n <= 100000;
 	}
 }
