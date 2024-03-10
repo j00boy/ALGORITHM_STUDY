93bd4b0175bba29202755c18265fd865ed46b484
j00boy <kkjjhh9705@gmail.com>
2024-03-11 00:47:53 +0900

    이친수

--- algorithm-by_myself/240311/src/boj_2193_이친수/Main.java
+++ algorithm-by_myself/240311/src/boj_2193_이친수/Main.java
@@ -1,7 +1,16 @@
 package boj_2193_이친수;
 
+import java.util.Scanner;
+
 public class Main {
+	
+	static int N;
+	static int[] D;
+	
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
 		
 	}
 }
