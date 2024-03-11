2aa4d2a231eee820db628c3189f504e1f5276f06
j00boy <kkjjhh9705@gmail.com>
2024-03-12 00:27:26 +0900

    계단 오르기 done

--- MorningStudy/week_07/src/boj_1912_연속합/Main.java
+++ MorningStudy/week_07/src/boj_1912_연속합/Main.java
@@ -1,5 +1,30 @@
+/**
+ * n개의 정수로 이루어진 임의의 수열
+ * 연속된 몇 개의 수를 선택해서 구할 수 있는 합 중 가장 큰 합
+ * - 몇개의 수 : 1 이상
+ * 이게 최장증가부분수열인가?
+ */
+
 package boj_1912_연속합;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	
+	static int N;
+	static int[] arr;
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();				// 1 <= N <= 100,000
+		arr = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();		// -1,000 <= arr[i] <= 1,000
+		}
+
+		
+	}
 }
--- MorningStudy/week_07/src/boj_2579_계단오르기/Main.java
+++ MorningStudy/week_07/src/boj_2579_계단오르기/Main.java
@@ -3,35 +3,47 @@ package boj_2579_계단오르기;
 import java.util.Scanner;
 
 public class Main {
-	
-	static int N;			// 계단의 개수 <= 300
-	static int[] D;		// 10000 * 300 = 3,000,000
-	static int[] scores;	// 300
-	
+
+	static int N; // 계단의 개수 <= 300
+	static int[] D; // 10000 * 300 = 3,000,000
+	static int[] scores; // 300
+
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		N = sc.nextInt();
-		D = new int[N+1];
-		scores = new int[N+1];
+		D = new int[N + 1];		
+		scores = new int[N + 1];
 		
-		for(int i = N; i >= 1; i--) {
+		/*
+		 * D[i] = i번째 칸을 밟았을 때 얻을 수 있는 최대 점수
+		 * - 조건1: 1칸 or 2칸
+		 * - 조건2: 3칸 연속 불가
+		 * - 조건3: 마지막 칸은 무조건 밟음 (N번째 칸)
+		 */
+
+		for (int i = 1; i <= N; i++) {
 			scores[i] = sc.nextInt();
 		}
 		
-		for(int i = N; i >= 1; i--) {
-			if(i == N) {
-				D[i] = scores[N];
-			} else if(i == N-1) {
-				D[i] = scores[N-1] + scores[N];
-			} else if(i == N-2) {
-				D[i] = Math.max(D[i+2] + scores[i], D[i+2] + scores[i+1]);
+		for(int i = 1; i <= N; i++) {
+			if(i == 1) {
+				D[1] = scores[1];
+			} else if(i == 2) {
+				D[2] = scores[1] + scores[2];
+			} else if(i == 3) {
+				D[3] = Math.max(scores[1] + scores[3], scores[2] + scores[3]);
 			} else {
-				D[i] = Math.max(D[i+3] + scores[i+2] + scores[i], D[i+3] + scores[i+1] + scores[i]);
+				// O X O O / ? O X O
+				// i-2번쨰 + i번째 밟기 vs i-3번째 + i-1번쨰 + i번째 밟기
+				D[i] = Math.max(D[i-3] + scores[i-1] + scores[i], D[i-2] + scores[i]);
 			}
 		}
-		
-		System.out.println(D[1]);
+
+
+		System.out.println(D[N]);
 	}
+	
+	
 }
