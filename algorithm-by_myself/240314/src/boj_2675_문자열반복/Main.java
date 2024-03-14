6aebeffe78eeb5c50c762a9b68253659bf0438f7
kkjjhh06 <kkjjhh06@naver.com>
2024-03-14 09:44:10 +0900

    240314 class 1

---
+++ algorithm-by_myself/240314/src/boj_2675_문자열반복/Main.java
@@ -0,0 +1,28 @@
+package boj_2675_문자열반복;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int tc = 1; tc <= T; tc++) {
+			int R = sc.nextInt();
+			String S = sc.next();
+			
+			String output = "";
+			
+			for(int i = 0; i < S.length(); i++) {
+				for(int j = 0; j < R; j++) {
+					output += S.charAt(i)+"";
+				}
+			}
+			
+			System.out.println(output);
+		}
+	}
+
+}
