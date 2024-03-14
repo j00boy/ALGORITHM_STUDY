8e440eab01967c45088dad811eef492c2cd7fc9c
kkjjhh06 <kkjjhh06@naver.com>
2024-03-15 08:46:19 +0900

    이제 늦게와야겠다

--- MorningStudy/week_08/src/boj_1931_회의실배정/Main.java
+++ MorningStudy/week_08/src/boj_1931_회의실배정/Main.java
@@ -1,5 +1,7 @@
 package boj_1931_회의실배정;
 
 public class Main {
-
+	public static void main(String[] args) {
+		
+	}
 }
--- MorningStudy/week_08/src/boj_2839_설탕배달/Main.java
+++ MorningStudy/week_08/src/boj_2839_설탕배달/Main.java
@@ -1,5 +1,25 @@
+/**
+ * [실버 4]
+ * - 설탕 배달
+ * - 3kg
+ * - 5kg
+ */
+
 package boj_2839_설탕배달;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	
+	static int N;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		
+		if(N % 5 == 0) {
+			System.out.println(N/5);
+		}
+	}
 }
