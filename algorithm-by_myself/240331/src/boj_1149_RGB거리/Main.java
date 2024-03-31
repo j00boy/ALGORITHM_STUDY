88f10c8ac529aeabf1e31866e6de156346fe9def
j00boy <kkjjhh9705@gmail.com>
2024-04-01 00:40:37 +0900

    rgb

--- algorithm-by_myself/240331/src/boj_1149_RGB거리/Main.java
+++ algorithm-by_myself/240331/src/boj_1149_RGB거리/Main.java
@@ -1,6 +1,9 @@
 package boj_1149_RGB거리;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	
@@ -10,26 +13,26 @@ public class Main {
 	static int min = Integer.MAX_VALUE;
 	static int sum = 0;
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+//		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
 		rgb = new int[N][3];
 		visited = new boolean[N][3];
 		
 		for(int r = 0; r < N; r++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int c = 0; c < 3; c++) {
-				rgb[r][c] = sc.nextInt();
+				rgb[r][c] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		int sum  = 0;
 		for(int c = 0; c < 3; c++) {
 			visited[0][c] = true;
-			sum += rgb[0][c];
 			permutation(1);
 			visited[0][c] = false;
-			sum -= rgb[0][c];
 		}
+		
 		
 		System.out.println(min);
 		
@@ -42,11 +45,14 @@ public class Main {
 			for(int r = 0; r < N; r++) {
 				for(int c = 0; c < 3; c++) {
 					if(visited[r][c]) {
+						if(sum + rgb[r][c] > min) {
+							return;
+						}
 						sum += rgb[r][c];
 					}
 				}
 			}
-			min = Math.min(min, sum);
+			min = sum;
 			return;
 		}
 		
