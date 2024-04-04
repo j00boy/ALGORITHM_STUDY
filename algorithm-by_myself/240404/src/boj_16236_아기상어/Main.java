b1ac759dcef0e95734bb25e86ee14e23f7efa4ed
j00boy <kkjjhh9705@gmail.com>
2024-04-04 10:30:12 +0900

    트리 순회 solved

--- algorithm-by_myself/240404/src/boj_16236_아기상어/Main.java
+++ algorithm-by_myself/240404/src/boj_16236_아기상어/Main.java
@@ -1,7 +1,24 @@
 package boj_16236_아기상어;
 
+import java.util.Scanner;
+
 public class Main {
+	
+	static int N;
+	static int[][] pool;
+	
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		pool = new int[N][N];
+		
+		for(int r = 0; r < N; r++) {
+			for(int c = 0; c < N; c++) {
+				pool[r][c] = sc.nextInt();
+			}
+		}
+		
 		
 	}
 }
