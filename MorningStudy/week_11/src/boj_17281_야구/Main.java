201ee9d2b8edc80dc87a73be63c4db6a518a33ad
j00boy <kkjjhh9705@gmail.com>
2024-04-08 09:16:59 +0900

    17281 야구

--- MorningStudy/week_11/src/boj_17281_야구/Main.java
+++ MorningStudy/week_11/src/boj_17281_야구/Main.java
@@ -1,5 +1,11 @@
 package boj_17281_야구;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
 }
