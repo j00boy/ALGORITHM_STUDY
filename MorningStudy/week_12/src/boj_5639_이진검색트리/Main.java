ccb64594a904005918fa2b6215e7e0549c3a1bb8
j00boy <kkjjhh9705@gmail.com>
2024-04-11 16:03:14 +0900

    240411

--- MorningStudy/week_12/src/boj_5639_이진검색트리/Main.java
+++ MorningStudy/week_12/src/boj_5639_이진검색트리/Main.java
@@ -1,5 +1,11 @@
 package boj_5639_이진검색트리;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
 }
