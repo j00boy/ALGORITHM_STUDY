880d14d7d16a92ac95d8308a0edaea5ad248e519
j00boy <kkjjhh9705@gmail.com>
2024-04-18 11:25:49 +0900

    경사로 푸는중

--- MorningStudy/week_13/src/boj_14890_경사로/Main2.java
+++ MorningStudy/week_13/src/boj_14890_경사로/Main2.java
@@ -2,7 +2,7 @@ package boj_14890_경사로;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main2 {
 	
 	static int N;
 	static int L;
@@ -26,28 +26,32 @@ public class Main {
 		
 		// 행 검사
 		for(int r = 0; r < N; r++) {
+			int idx = 0;
 			int hgt = map[r][0];
 			int count = 1;
 			boolean canGo = true;
-			for(int c = 1; c < N; c++) {
-				if(map[r][c] == hgt) {
+			
+			while(idx < N) {
+				if(map[r][idx] == hgt) {
 					count++;
-				} else if(Math.abs(map[r][c] - hgt) > 1) {
+					idx++;
+				} else if(Math.abs(map[r][idx] - hgt) > 1) {
 					canGo = false;
 					break;
-				} else if((map[r][c] - hgt) == 1) {	// 지금 나온게 더 클 때
+				} else if((map[r][idx] - hgt) == 1) {	// 지금 나온게 더 클 때
 					if(count > L) {
-						hgt = map[r][c];
+						hgt = map[r][idx];
 						count = 1;
+						idx++;
 					} else {
 						canGo = false;
 						break;
 					}
-				} else if((map[r][c] - hgt) == -1) {
-					if(check(r, c+L)) {
+				} else if((map[r][idx] - hgt) == -1) {
+					if(check(r, idx+L)) {
 						boolean flag = true;
-						for(int i = c; i < c+L; i++) {
-							if(map[r][i] != map[r][c]) {
+						for(int i = idx; i < idx+L; i++) {
+							if(map[r][i] != map[r][idx]) {
 								flag = false;
 								break;
 							}
@@ -56,42 +60,48 @@ public class Main {
 							canGo = false;
 							break;
 						}
-						hgt = map[r][c];
-						count = 1;
+						hgt = map[r][idx];
+						count = 0;
+						idx += L;
 					} else {
 						canGo = false;
 						break;
 					}
 				}
 			}
+			
 			if(canGo) road++;
 		}
 		
 		
 		// 열 검사
 		for(int c = 0; c < N; c++) {
+			int idx = 0;
 			int hgt = map[0][c];
 			int count = 1;
 			boolean canGo = true;
-			for(int r = 1; r < N; r++) {
-				if(map[r][c] == hgt) {
+			
+			while(idx < N) {
+				if(map[idx][c] == hgt) {
 					count++;
-				} else if(Math.abs(map[r][c] - hgt) > 1) {
+					idx++;
+				} else if(Math.abs(map[idx][c] - hgt) > 1) {
 					canGo = false;
 					break;
-				} else if((map[r][c] - hgt) == 1) {	// 지금 나온게 더 클 때
+				} else if((map[idx][c] - hgt) == 1) {	// 지금 나온게 더 클 때
 					if(count > L) {
-						hgt = map[r][c];
+						hgt = map[idx][c];
 						count = 1;
+						idx++;
 					} else {
 						canGo = false;
 						break;
 					}
-				} else if((map[r][c] - hgt) == -1) {
-					if(check(r, c+L)) {
+				} else if((map[idx][c] - hgt) == -1) {
+					if(check(idx + L, c)) {
 						boolean flag = true;
-						for(int i = c; i < c+L; i++) {
-							if(map[r][i] != map[r][c]) {
+						for(int i = idx; i < idx+L; i++) {
+							if(map[i][c] != map[idx][c]) {
 								flag = false;
 								break;
 							}
@@ -100,17 +110,19 @@ public class Main {
 							canGo = false;
 							break;
 						}
-						hgt = map[r][c];
-						count = 1;
+						hgt = map[idx][c];
+						count = 0;
+						idx += L;
 					} else {
 						canGo = false;
 						break;
 					}
 				}
 			}
+			
 			if(canGo) road++;
 		}
-		
+				
 		System.out.println(road);
 		
 	}
--- MorningStudy/week_13/src/boj_2293_동전1/Main.java
+++ MorningStudy/week_13/src/boj_2293_동전1/Main.java
@@ -3,9 +3,5 @@ package boj_2293_동전1;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		
-	}
+	
 }
