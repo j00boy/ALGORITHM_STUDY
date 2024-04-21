3eea7eb8917c17c52e9905f099e8d0c1dd1a19e1
j00boy <kkjjhh9705@gmail.com>
2024-04-21 23:34:45 +0900

    최대거리 solved

--- MorningStudy/week_13/src/boj_20057_마법사상어와토네이도/Main.java
+++ MorningStudy/week_13/src/boj_20057_마법사상어와토네이도/Main.java
@@ -1,5 +1,9 @@
 package boj_20057_마법사상어와토네이도;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	}
 }
