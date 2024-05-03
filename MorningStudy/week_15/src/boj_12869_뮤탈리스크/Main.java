cfb6aba4304331f7ac71d40384a597f6bab210eb
j00boy <kkjjhh9705@gmail.com>
2024-05-04 00:30:01 +0900

    원판 solved

--- MorningStudy/week_15/src/boj_12869_뮤탈리스크/Main.java
+++ MorningStudy/week_15/src/boj_12869_뮤탈리스크/Main.java
@@ -1,5 +1,18 @@
 package boj_12869_뮤탈리스크;
 
+import java.util.Scanner;
+
 public class Main {
+	
+	static int N;
+	static int[][][] scv;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		scv = new int[61][61][61];
+		N = sc.nextInt();
+		
+	}
 
 }
