4220d02b4d69a8b4ab7b398a8a55df34c55eb508
j00boy <kkjjhh9705@gmail.com>
2024-05-05 22:18:45 +0900

    mvc

--- MorningStudy/week_15/src/boj_13460_구슬탈출2/Main.java
+++ MorningStudy/week_15/src/boj_13460_구슬탈출2/Main.java
@@ -1,5 +1,11 @@
 package boj_13460_구슬탈출2;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
 }
