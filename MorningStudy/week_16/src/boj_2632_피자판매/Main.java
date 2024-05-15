09613ad7a1b498a52a155b9a302cd8ec0f6b6241
j00boy <kkjjhh9705@gmail.com>
2024-05-15 21:27:01 +0900

    pizza

--- MorningStudy/week_16/src/boj_2632_피자판매/Main.java
+++ MorningStudy/week_16/src/boj_2632_피자판매/Main.java
@@ -1,5 +1,44 @@
 package boj_2632_피자판매;
 
-public class Main {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Main {
+	
+	static int order;
+	static int n;
+	static int m;
+	static int[] A;
+	static int[] B;
+	static int[] a;
+	static int[] b;
+	
+	static int way;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		order = sc.nextInt();
+		n = sc.nextInt();
+		m = sc.nextInt();
+		
+		A = new int[n];
+		B = new int[m];
+		
+		way = 0;
+		
+		for(int i = 1; i < n; i++) {
+			A[i] = sc.nextInt();
+		}
+		
+		for(int i = 1; i < m; i++) {
+			B[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(A);
+		Arrays.sort(B);
+		
+		
+		System.out.println(way);
+	}
 }
