081efa7ce48482fb3839f6a64b16fe16486b4c59
j00boy <kkjjhh9705@gmail.com>
2024-07-08 22:27:16 +0900

    부분합 푸는중

--- MorningStudy/week_20/src/boj_1806_부분합/Main.java
+++ MorningStudy/week_20/src/boj_1806_부분합/Main.java
@@ -1,11 +1,52 @@
 package boj_1806_부분합;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+	
+	static int N;
+	static int S;
+	static int[] arr;
+	static int leng;
+	
 	public static void main(String[] args) {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		S = sc.nextInt();
+		arr = new int[N];
+		leng = 0;
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+			if(arr[i] >= S) {
+				leng = 1;
+			}
+		}
+		
+		if(leng != 0) {
+			System.out.println(leng);
+			return;
+		}
+
+		// 누적합
+		int sum = 0;
+		
+		int[] sumArr = new int[N+1];
+		for(int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+			sumArr[i + 1] = sum;
+		}
+		
+//		System.out.println(Arrays.toString(sumArr));
+		if(sum < S) {
+			System.out.println(0);
+			return;
+		}
+		
+		
+		
 		
 	}
 }
