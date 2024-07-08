46555e4082dd1f8f0ec4d5df141f35fdf7fc9939
j00boy <kkjjhh9705@gmail.com>
2024-07-08 21:50:06 +0900

    week 20

---
+++ MorningStudy/week_20/src/boj_1238_파티/Main.java
@@ -0,0 +1,10 @@
+package boj_1238_파티;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
