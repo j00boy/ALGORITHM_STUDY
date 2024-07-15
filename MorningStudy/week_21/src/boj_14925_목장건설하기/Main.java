bf582521f0085d0f795b9c4ca55bb9901aafdb90
j00boy <kkjjhh9705@gmail.com>
2024-07-15 23:14:52 +0900

    목장

--- MorningStudy/week_21/src/boj_14925_목장건설하기/Main.java
+++ MorningStudy/week_21/src/boj_14925_목장건설하기/Main.java
@@ -1,5 +1,6 @@
 package boj_14925_목장건설하기;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -9,6 +10,8 @@ public class Main {
 	static int[][] map;
 	static int max;
 	
+	static int[][] D;
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
@@ -16,6 +19,7 @@ public class Main {
 		N = sc.nextInt();
 		
 		map = new int[M][N];
+		D = new int[M][N];
 		max = 0;
 		
 		for(int r = 0; r < M; r++) {
@@ -24,7 +28,53 @@ public class Main {
 			}
 		}
 		
+		// 첫 열
+		int can = 1;
+		for(int c = 0; c < N; c++) {
+			if(map[0][c] == 0) {
+				D[0][c] = can;
+				can++;
+			} else {
+				D[0][c] = 0;
+				can = 1;
+			}
+		}
 		
+//		System.out.println(Arrays.toString(D[0]));
+		
+		// 첫 행
+		can = 1;
+		for(int r = 0; r < M; r++) {
+			if(map[r][0] == 0) {
+				D[r][0] = can;
+				can++;
+			} else {
+				D[r][0] = 0;
+				can = 1;
+			}
+		}
+		
+		for(int r = 1; r < M; r++) {
+			for(int c = 1; c < N; c++) {
+				if(map[r][c] == 1) {
+					D[r][c] = 0;
+				} else if(map[r][c] == 2) {
+					D[r][c] = 0;
+				} else {	// 목장을 지을 수 있을 때
+					D[r][c] = Math.min(Math.min(D[r-1][c] + 1, D[r][c-1] + 1), D[r-1][c-1] + 1);
+					max = Math.max(max, D[r][c]);
+				}
+			}
+		}
+		
+		System.out.println(max);
+		
+//		for(int r = 0; r < M; r++) {
+//			for(int c = 0; c < N; c++) {
+//				System.out.print(D[r][c] + " ");
+//			}
+//			System.out.println();
+//		}
 		
 	}
 }
