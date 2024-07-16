991416acd07444b9b7f65a41c897f5869ecc6527
kkjjhh06 <kkjjhh06@naver.com>
2024-07-16 09:00:45 +0900

    공항

--- MorningStudy/week_21/src/boj_10775_공항/Main.java
+++ MorningStudy/week_21/src/boj_10775_공항/Main.java
@@ -1,5 +1,12 @@
 package boj_10775_공항;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		// 오랜만에 연습해보고 싶었음
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
 }
--- MorningStudy/week_21/src/boj_14925_목장건설하기/Main.java
+++ MorningStudy/week_21/src/boj_14925_목장건설하기/Main.java
@@ -1,6 +1,5 @@
 package boj_14925_목장건설하기;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -27,53 +26,39 @@ public class Main {
 				map[r][c] = sc.nextInt();
 			}
 		}
-		
-		// 첫 열
-		int can = 1;
-		for(int c = 0; c < N; c++) {
-			if(map[0][c] == 0) {
-				D[0][c] = can;
-				can++;
-			} else {
-				D[0][c] = 0;
-				can = 1;
-			}
-		}
-		
-//		System.out.println(Arrays.toString(D[0]));
-		
-		// 첫 행
-		can = 1;
+
 		for(int r = 0; r < M; r++) {
-			if(map[r][0] == 0) {
-				D[r][0] = can;
-				can++;
-			} else {
-				D[r][0] = 0;
-				can = 1;
-			}
-		}
-		
-		for(int r = 1; r < M; r++) {
-			for(int c = 1; c < N; c++) {
+			for(int c = 0; c < N; c++) {
 				if(map[r][c] == 1) {
 					D[r][c] = 0;
 				} else if(map[r][c] == 2) {
 					D[r][c] = 0;
-				} else {	// 목장을 지을 수 있을 때
-					D[r][c] = Math.min(Math.min(D[r-1][c] + 1, D[r][c-1] + 1), D[r-1][c-1] + 1);
+				} else {
+					/*
+					 *  양변이 0일때는 D[r-1][c] or D[r][c-1]이랑 비교를 할 수가 없고,
+					 *  내 기준은 정사각형이 만들어지는 가장 오른쪽 꼭짓점이 기준이기 때문에
+					 *  해당 변에서는 무조건 1이 최고 길이임
+					 */
+					if(r == 0) D[r][c] = 1;
+					else if(c == 0) D[r][c] = 1;
+					else {
+						D[r][c] = Math.min(Math.min(D[r-1][c], D[r][c-1]) + 1, D[r-1][c-1] + 1);
+					}
+					max = Math.max(max, D[r][c]);
 				}
 			}
 		}
 		
-//		System.out.println(max);
-		
-		for(int r = 0; r < M; r++) {
-			for(int c = 0; c < N; c++) {
-				System.out.print(D[r][c] + " ");
+//		print(D);
+		System.out.println(max);
+	}
+	
+	public static void print(int[][] arr) {
+		for(int r = 0; r < arr.length; r++) {
+			for(int c = 0; c < arr[0].length; c++) {
+				System.out.print(arr[r][c] + " ");
 			}
 			System.out.println();
 		}
-		
 	}
 }
