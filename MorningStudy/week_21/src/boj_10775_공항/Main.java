4f12a86849617180e1a0f049933ac9f7530aa363
j00boy <kkjjhh9705@gmail.com>
2024-07-16 23:40:07 +0900

    보석도둑 solved

--- MorningStudy/week_21/src/boj_10775_공항/Main.java
+++ MorningStudy/week_21/src/boj_10775_공항/Main.java
@@ -1,12 +1,46 @@
 package boj_10775_공항;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) {
+	
+	static int G;
+	static int P;
+	static boolean[] isFull;
+	static int count;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
 		// 오랜만에 연습해보고 싶었음
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		G = Integer.parseInt(br.readLine());
+		P = Integer.parseInt(br.readLine());
+		
+		isFull = new boolean[G+1];
+		count = 0;
+		
+		for(int i = 0; i < P; i++) {
+			int gateNo = Integer.parseInt(br.readLine());
+			
+			findGate(gateNo);
+			
+		}
+//		System.out.println(Arrays.toString(isFull));
+		
+		System.out.println(count);
+	}
+	
+	public static void findGate(int gateNo) {
+		for(int i = gateNo; i >= 1; i--) {
+			if(!isFull[i]) {
+				isFull[i] = true;
+				count++;
+				break;
+			}
+		}
 	}
 }
