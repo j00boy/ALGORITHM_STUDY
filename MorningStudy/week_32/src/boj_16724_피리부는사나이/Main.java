6e1f54543aaf5d22c94b6f2c120e6615c68975b5
kkjjhh06 <kkjjhh06@naver.com>
2024-10-24 09:36:37 +0900

    [week32] 피리부는사나이 solve

--- MorningStudy/week_32/src/boj_16724_피리부는사나이/Main.java
+++ MorningStudy/week_32/src/boj_16724_피리부는사나이/Main.java
@@ -2,82 +2,91 @@ package boj_16724_피리부는사나이;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-	
+
 	static int N;
 	static int M;
 	static char[][] map;
 	static int[][] zone;
-	
+
 	public static void main(String[] args) throws Exception {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		
+
 		map = new char[N][M];
 		zone = new int[N][M];
-		
-		for(int r = 0; r < N; r++) {
+
+		for (int r = 0; r < N; r++) {
 			char[] arr = br.readLine().toCharArray();
-			for(int c = 0; c < M; c++) {
+			for (int c = 0; c < M; c++) {
 				map[r][c] = arr[c];
 			}
 		}
-		
-		// 구역을 찾기
-		int cnt = 0;
-		for(int r = 0; r < N; r++) {
-			for(int c = 0; c < M; c++) {
-				if(zone[r][c] == 0) {
-					zone[r][c] = ++cnt;
-//					System.out.println("cnt: " + cnt);
-					int row = r;
-					int col = c;
-					while(true) {
-						switch(map[row][col]) {
-							case 'U': row -= 1; break;
-							case 'D': row += 1; break;
-							case 'L': col -= 1; break;
-							default: col += 1; break;
-						}
-						
-						if(zone[row][col] != 0) {
-							break;
-						} else {
-							zone[row][col] = cnt;
-						}
-					}
+
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < M; c++) {
+				zone[r][c] = 10000 * r + c;
+			}
+		}
+
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < M; c++) {
+				int row = r;
+				int col = c;
+				switch (map[r][c]) {
+				case 'U':
+					row -= 1;
+					break;
+				case 'D':
+					row += 1;
+					break;
+				case 'L':
+					col -= 1;
+					break;
+				default:
+					col += 1;
+					break;
 				}
+				union(zone[r][c], zone[row][col]); 
 			}
 		}
 		
-		printZone();
-		
-		System.out.println(cnt);
-		
-		
-	}
-	
-	public static void printZone() {
-		for(int r = 0; r < N; r++) {
-			for(int c = 0; c < M; c++) {
-				System.out.print(zone[r][c]);
+		Set<Integer> set = new HashSet<>();
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < M; c++) {
+				set.add(find(zone[r][c]));
 			}
-			System.out.println();
 		}
+
+		System.out.println(set.size());
 	}
-	
-	public static void print() {
-		for(int r = 0; r < N; r++) {
-			for(int c = 0; c < M; c++) {
-				System.out.print(map[r][c]);
-			}
-			System.out.println();
-		}		
+
+	public static int find(int n) {
+		int r = n / 10000;
+		int c = n % 10000;
+
+		if (zone[r][c] != 10000 * r + c) {
+			zone[r][c] = find(zone[r][c]);
+		}
+
+		return zone[r][c];
+	}
+
+	public static void union(int a, int b) {
+
+		a = find(a);
+		b = find(b);
+
+		if (a != b) {
+			zone[b / 10000][b % 10000] = a;
+		}
 	}
 }
