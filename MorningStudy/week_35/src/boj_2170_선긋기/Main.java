736f2404ef63517095b69b586ce70b57017b6f1e
김주형 <josh@gimjuhyeong-ui-MacBookPro.local>
2024-11-23 19:21:22 +0900

    :sparkles:[Week_35] BOJ_2170_선긋기 초안 작성

---
+++ MorningStudy/week_35/src/boj_2170_선긋기/Main.java
@@ -0,0 +1,20 @@
+package boj_2170_선긋기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= N; i++) {
+
+        }
+
+    }
+}
