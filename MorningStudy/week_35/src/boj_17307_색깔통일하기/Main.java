2be6a10d4d05604ad71ec7cbcb1920e2a7c23245
kkjjhh9705 <kkjjhh9705@gmail.com>
2024-12-03 13:29:44 +0900

    :sparkles:[Feat] BOJ_17307_색깔통일하기 생성

---
+++ MorningStudy/week_35/src/boj_17307_색깔통일하기/Main.java
@@ -0,0 +1,24 @@
+package boj_17307_색깔통일하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        int N, C;
+        int[] arr;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+
+    }
+}
